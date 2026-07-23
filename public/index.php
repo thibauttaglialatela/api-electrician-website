9a0bae432c6f3c2f0eaff415d03a17573c1ddb65
thibauttaglialatela <thibauttaglialatela@gmail.com>
2026-07-23 13:47:07 +0200

    chore: :arrow_up: update to symfony 7.4

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once \dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-return function (array $context): Kernel 
-{
-    return new Kernel($context['APP_ENV'], $context['APP_DEBUG']);
+return static function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
